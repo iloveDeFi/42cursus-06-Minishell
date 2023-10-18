712a48634d78752c876f15f2fd4c36e0317a7bea
Baptiste Bessard <bat@air-de-baptiste.home>
2023-10-18 14:40:12 +0200

    structures

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: bat <bat@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 17:02:10 by bat               #+#    #+#             */
-/*   Updated: 2023/10/16 16:21:28 by bat              ###   ########.fr       */
+/*   Updated: 2023/10/18 14:26:28 by bat              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,66 @@ typedef enum
 
 // Excecution  
 
-typedef struct EnvList 
-{
-    struct EnvVar *head;
-}   t_EnvList;
 
 // libft
 
 
 // Linked Lists
-typedef struct s_Env
+
+
+// Parsing
+
+typedef struct s_argList
+{
+    int listLength;
+    struct s_argList *head;
+    struct s_argList *tail;
+} t_argList;
+typedef struct s_arg
+{
+    char *name;
+    struct s_argList *next;
+    struct s_argList *prev;
+}   t_arg;
+
+typedef struct s_commandList
+{
+    int commandListLength;
+    char *name;
+    struct s_command *head;
+    struct s_command *tail;
+}   t_commandList;
+typedef struct s_command
+{
+    char *name;
+    char **arguments;
+    struct s_command *head;
+    struct s_command *tail;// Un tableau de chaînes pour les arguments
+}   t_command;
+
+typedef struct envVarList 
+{
+    int envVarListLength;
+    struct envVar *head;
+    struct envVar *tail;
+}   t_envList;
+typedef struct s_envVar
 {
     char *name;
     char *value;
-    struct s_EnvVar *next;
-    struct s_EnvVar *prev;
+    struct s_envVar *next;
+    struct s_envVar *prev;
 }   t_env;  
 
-// Parsing
+typedef struct s_Redir
+{
+    char *file_name;
+    t_type  type;
+    struct s_redir *next;
+    struct s_redir *prev;
+
+}   t_redir;
+
 
 // Signals
 
