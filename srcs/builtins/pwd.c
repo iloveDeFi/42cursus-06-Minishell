7eb3368c8ffb035b6f29acbd0812e2c25fc781f1
BeldaJulien <beldajulien7@gmail.com>
2023-10-22 11:24:57 +0200

    fix problem

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -3,11 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bat <bat@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 01:12:23 by bat               #+#    #+#             */
-/*   Updated: 2023/09/28 01:12:26 by bat              ###   ########.fr       */
+/*   Updated: 2023/10/20 19:53:28 by julien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	pwd(void)
+{
+	char	path[PATH_MAX];
+
+	if (getcwd(path, PATH_MAX) != NULL)
+		printf("%s\n", path);
+	else
+		printf("[ERROR] : PWD command failed");
+}
