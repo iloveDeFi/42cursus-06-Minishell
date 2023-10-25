b3d3e407f8548fbeadec6d302808ab7c51026c9d
Baptiste Bessard <bat@pwlan-1984.unil.ch>
2023-10-25 16:40:18 +0200

    main file - readline instead of fgets

--- includes/minishell.h
+++ includes/minishell.h
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <errno.h>
 #include <limits.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 # define  MAX_INPUT_SIZE 1024
 
@@ -24,7 +26,6 @@ typedef enum
 
 // Excecution  
 
-
 // libft
 
 
@@ -32,27 +33,21 @@ typedef enum
 
 
 // Parsing
+int	ft_str_error(char *str, int number);
 
-typedef struct s_argList
-{
-    int listLength;
-    struct s_argList *head;
-    struct s_argList *tail;
-} t_argList;
 typedef struct s_arg
 {
     char *name;
-    struct s_argList *next;
-    struct s_argList *prev;
+    struct s_arg *next;
+    struct s_arg *prev;
 }   t_arg;
-
-typedef struct s_commandList
+typedef struct s_argList
 {
-    int commandListLength;
-    char *name;
-    struct s_command *head;
-    struct s_command *tail;
-}   t_commandList;
+    int listLength;
+    struct t_arg *head;
+    struct t_arg *tail;
+} t_argList;
+
 typedef struct s_command
 {
     char *name;
@@ -60,13 +55,14 @@ typedef struct s_command
     struct s_command *head;
     struct s_command *tail;// Un tableau de chaînes pour les arguments
 }   t_command;
-
-typedef struct envVarList 
+typedef struct s_commandList
 {
-    int envVarListLength;
-    struct envVar *head;
-    struct envVar *tail;
-}   t_envList;
+    int commandListLength;
+    char *name;
+    struct t_command *head;
+    struct t_command *tail;
+}   t_commandList;
+
 typedef struct s_envVar
 {
     char *name;
@@ -74,14 +70,19 @@ typedef struct s_envVar
     struct s_envVar *next;
     struct s_envVar *prev;
 }   t_env;  
+typedef struct s_envVarList 
+{
+    int envVarListLength;
+    struct t_envVar *head;
+    struct t_envVar *tail;
+}   t_envList;
 
-typedef struct s_Redir
+typedef struct s_redir
 {
     char *file_name;
-    t_type  type;
+    int redirection_type;
     struct s_redir *next;
     struct s_redir *prev;
-
 }   t_redir;
 
 
