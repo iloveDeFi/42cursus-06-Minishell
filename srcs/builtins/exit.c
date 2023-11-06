1be4f09039230e93b4acabb9f9815d341c5bc358
julien Belda <julienbelda@MacBook-Pro-de-julien.local>
2023-11-06 10:26:03 +0100

    exit, unset

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -3,11 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bat <bat@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: julienbelda <julienbelda@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 01:12:14 by bat               #+#    #+#             */
-/*   Updated: 2023/09/28 01:12:16 by bat              ###   ########.fr       */
+/*   Updated: 2023/11/04 10:19:14 by julienbelda      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+#include "minishell.h"
+
+// Fonction pour gérer la commande "exit"
+void exit(t_command *command) 
+{
+    int exit_code;
+    
+    exit_code = ft_atoi(command->arguments[1]);
+    if (command->arguments[1] != NULL) 
+    {   
+        if (exit_code == 0) 
+            exit(exit_code);
+        else 
+        {
+            printf(stderr, "Invalid exit code: %s\n", command->arguments[1]);
+            exit(EXIT_FAILURE);
+        }
+    } 
+    else 
+        exit(0);
+}
--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -3,11 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bat <bat@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: julienbelda <julienbelda@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 01:12:29 by bat               #+#    #+#             */
-/*   Updated: 2023/09/28 01:12:32 by bat              ###   ########.fr       */
+/*   Updated: 2023/11/06 10:22:35 by julienbelda      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int unset(t_global *global, char *arg) 
+{
+    int exit_code = 0;
+    t_envVar *tmp = global->envVars->head;
+    t_envVar *prev = NULL;
+
+    while (tmp) 
+    {
+        if (strcmp(arg, tmp->name) == 0) 
+        {
+            if (prev == NULL)
+                global->envVars->head = tmp->next;
+            else
+                prev->next = tmp->next;
+            free(tmp->name);
+            free(tmp->value);
+            free(tmp);
+            break;
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    if (!is_valid_identifier(arg)) 
+        exit_code = EXIT_FAILURE;
+    if (exit_code != 0)
+        return exit_code;
+    return EXIT_SUCCESS;
+}
+
+
+// Helper function to check if a string is a valid identifier
+int is_valid_identifier(const char *name)
+{
+    int i;
+
+    i = 1;
+    if (!name || !name[0] || !isalpha(name[0]))
+        return FALSE;
+    while (name[i])
+    {
+        if (!isalnum(name[i]) && name[i] != '_')
+            return FALSE;
+        i++;
+    }
+    return TRUE;
+}
