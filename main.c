69d82ce70602c242d799837c6b34976bf609b234
Baptiste Bessard <bessardbusiness@gmail.com>
2023-11-18 15:03:24 +0100

    tokenize with quotes

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ int main(int ac, char **av, char **envp)
             av[i] = malloc(ft_strlen(token) + 1);
             ft_strcpy(av[i], token);
             ft_strtrim_with_quotes(token);
-            //ft_tokenize_with_quotes(token);
+            ft_tokenize_with_quotes(token);
             token = ft_strtok(NULL, " \n");
             i++;
         }
