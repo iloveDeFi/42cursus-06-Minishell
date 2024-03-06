7b73b93f50769387c2b68310cd0f02c5659fff14
Baptiste Bessard <bessardbusiness@gmail.com>
2024-03-07 00:28:35 +0100

    parser

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -14,11 +14,8 @@ t_command *ft_parser(char * input, t_env *envList)
 	tokens = ft_tokenize_input_with_strtok(input); // TODO rename this function
 	if (!ft_parse_tokens(&commands, tokens))
 	{
-		// ? TODO something to free?
 		return NULL;
 	}
-	// ft_parse_all_redirection(*tokens);
-	// ? TODO should the replace usd / quotes be done in the tokenize function instead?
 	ft_token_is_a_quotes(input);
 	ft_found_and_replace_usd(commands, envList);
 	return commands;
